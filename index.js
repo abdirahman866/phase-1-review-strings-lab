845560965fd1470bbf21d8e43ac41b3e19eb8207
Abdirahman <abdirahman.salah@student.moringaschool.com>
2025-03-18 11:57:41 +0300

    update

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Write your code in this file!
+// Define the currentUser variable with any name you like
+const currentUser = 'Grace Hopper';
+
+// Create the welcomeMessage using template literals
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+// Make an excited version by making welcomeMessage uppercase
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+// Create a short greeting that only uses the first letter of currentUser
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+
